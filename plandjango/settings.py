892bb37b826b6fa82e241c3091cfa6380e3a666c
nnlangdo <nksproday@gmail.com>
2022-02-02 15:12:46 +0530

    allowed hosts

--- plandjango/settings.py
+++ plandjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s65glttfp8_935obn1fgpyyf!01efm^gpp!7$yepa)pv%ez@ab
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
